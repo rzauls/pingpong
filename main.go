d4baee98c01f5326bfaee1093c9ba07629b558d8
Rihards <rihards.zauls@gmail.com>
2023-03-24 11:39:02 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port, err := getPort()
+	if err != nil {
+		log.Fatalf("failed to parse http port: %s", err)
+	}
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(fmt.Sprintf(":%d", port))
+}
+
+func getPort() (int, error) {
+	val := os.Getenv("SERVER_PORT")
+	if len(val) == 0 {
+		return 8081, nil
+	}
+	return strconv.Atoi(val)
+}
